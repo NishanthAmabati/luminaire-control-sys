c1af286128cbd7cab0994cb6a519ab1a1e00584c
Yagnya <yagnya.nishanth@gmail.com>
2026-07-05 16:32:54 +0530

    feat: add non-root user to all Dockerfiles

--- webapp/src/hooks/useTimerControl.ts
+++ webapp/src/hooks/useTimerControl.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useUiFeedback } from '../context/useUiFeedback';
 import { readErrorMessage, unknownToMessage } from '../utils/apiError';
 
